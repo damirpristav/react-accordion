b65c6a7675ff02fb558ffbb38c3c44cf50911445
Damir Pristav <damirpristav@gmail.com>
2020-08-08 12:11:28 +0200

    readme file update

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
     <Fragment>
       <Header title="React JS accordions" />
       <div className="container">
-        <Accordions data={accordionData} duration={2000} opened={2} />
+        <Accordions data={accordionData} duration={400} opened={0} />
       </div>
     </Fragment>
   );
